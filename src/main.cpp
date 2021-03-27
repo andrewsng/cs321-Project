4ac8d2e79ead198100e48d52e3213ea329e4f030
Andrew Ng <ngandrew1337@gmail.com>
2021-03-27 15:27:58 -0800

    Plot a pixel using BIOS interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,14 @@ int main()
     regs.h.al = 0x13;
     int86(0x10, &regs, &regs);
     
+    const int color = 10;
+    regs.h.ah = 0x0C;
+    regs.h.al = color;
+    regs.x.cx = 160;
+    regs.x.dx = 100;
+    int86(0x10, &regs, &regs);
+    
+    // wait ~1 second
     int sum = 0;
     for (int i = 0; i < 100000000; ++i)
     {
