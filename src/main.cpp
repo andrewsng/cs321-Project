2840505fb4cfe30022ef2d5384f4a7e24ef7ae2d
Andrew Ng <ngandrew1337@gmail.com>
2021-03-27 16:15:33 -0800

    Display default VGA color palette

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,30 @@ int main()
     regs.h.al = 0x13;
     int86(0x10, &regs, &regs);
     
+    const int width = 320;
+    const int height = 200;
+    
     const int color = 10;
     regs.h.ah = 0x0C;
     regs.h.al = color;
-    regs.x.cx = 160;
-    regs.x.dx = 100;
+    regs.x.cx = width / 2;
+    regs.x.dx = height / 2;
     int86(0x10, &regs, &regs);
     
     uint8_t *VGA = (uint8_t *)0xA0000 + __djgpp_conventional_base;
     const int x = 200;
-    const int y = 100;
-    const int offset = y * 320 + x;
+    const int y = height / 2;
+    const int offset = y * width + x;
     VGA[offset] = 12;
+    
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            const int offset = y * width + x;
+            VGA[offset] = x % 256;
+        }
+    }
     
     // wait ~1 second
     int sum = 0;
