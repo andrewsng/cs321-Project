46b7d5e36541198d0e007cecb653756dda5f5c4f
Andrew Ng <ngandrew1337@gmail.com>
2021-04-01 16:18:54 -0800

    polygon.h: Create initial structs for Points/Scanlines

---
+++ polygon.h
@@ -0,0 +1,40 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+#include <array>
+
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+
+struct PointList
+{
+    int length;
+    Point *points;
+};
+
+
+struct Scanline
+{
+    int xStart;
+    int xEnd;
+};
+
+
+struct ScanlineList
+{
+    int length;
+    int yStart;
+    Scanline *lines;
+};
+
+
+bool fillConvexPolygon(const PointList& vertices, int color, int xOffset, int yOffset);
+
+
+#endif  // POLYGON_H
+
